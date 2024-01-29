fba0c4c6d3ce26d94fb3ed4451ed392e4b21357d
Leonardo Maia Jr <leomaiajr@hotmail.com>
2024-01-29 17:34:46 -0300

    feat: add global meta and error boundary

--- app/root.tsx
+++ app/root.tsx
@@ -3,12 +3,22 @@ import {
   Links,
   LiveReload,
   Meta,
+  type MetaFunction,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
+  isRouteErrorResponse,
 } from '@remix-run/react';
 import stylesheet from '~/tailwind.css';
 import AppBar from './components/AppBar/AppBar';
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'TaskTrek' },
+    { name: 'description', content: 'Simple Task management app' },
+  ];
+};
 
 export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: stylesheet },
@@ -29,6 +39,30 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <html lang="en">
+      <head>
+        <title>Oh no!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>Oh no!</h1>
+        {isRouteErrorResponse(error) ? (
+          <p>{error.status} Error</p>
+        ) : (
+          <p>Something went wrong.</p>
+        )}
+        <Scripts />
       </body>
     </html>
   );
